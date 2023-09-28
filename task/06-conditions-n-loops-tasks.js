e58614ec96a065d0055b5380720bf0194f0247f2
EugeneTsalko <tefworkmail@gmail.com>
2023-09-28 18:28:53 +0300

    solve: findFirstSingleChar

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -188,7 +188,14 @@ function isInsideCircle(circle, point) {
  *   'entente' => null
  */
 function findFirstSingleChar(str) {
-    throw new Error('Not implemented');
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (str.indexOf(char) === i && str.indexOf(char, i + 1) === -1) {
+          return char;
+        }
+      }
+
+    return null;
 }
 
 
