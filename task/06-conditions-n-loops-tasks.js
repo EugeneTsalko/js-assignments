838af3a659ca89e98d60fe2b1cf48c4bf7d581c0
EugeneTsalko <tefworkmail@gmail.com>
2023-09-28 18:13:16 +0300

    solve: getSumBetweenNumbers

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -75,7 +75,13 @@ function getFactorial(n) {
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
 function getSumBetweenNumbers(n1, n2) {
-    throw new Error('Not implemented');
+    let result = 0;
+
+    for (let i = n1; i <= n2; i++) {
+        result += i;
+    }
+
+    return result;
 }
 
 
