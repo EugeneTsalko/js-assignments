8000b583978064bc65616fcaaf67e0cdc1775f0e
EugeneTsalko <tefworkmail@gmail.com>
2023-10-02 18:40:58 +0300

    refactor: selectMany

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -538,7 +538,7 @@ function group(array, keySelector, valueSelector) {
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-  return arr.flatMap((el) => childrenSelector(el));
+  return arr.flatMap(childrenSelector);
 }
 
 /**
